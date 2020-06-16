b2c038bdcaa2bf989c6147d1777187353022d92b
zoulei <337544076@qq.com>
2020-06-16 09:56:06 +0800

    my tijiao

---
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,9 @@
+package com.atguigu.git;public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("hello git!");
+int a=19; 
+int b =20;
+System.out.printLn(a+b);
+	
+	}
+}
